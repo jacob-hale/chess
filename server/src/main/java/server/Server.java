6a5940d2efa460c4f289d8fe6a58aff8e023a960
Jacob Hale <Jchale23@byui.edu>
2025-03-03 14:48:36 -0700

    server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,8 @@
 package server;
 
+import dataaccess.MemoryUserDAO;
+import dataaccess.MemoryAuthDAO;
+import service.UserService;
 import spark.*;
 
 public class Server {
@@ -10,9 +13,12 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        var userDAO = new MemoryUserDAO();
+        var authDAO = new MemoryAuthDAO();
+        var userService = new UserService(userDAO, authDAO);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+        Spark.post("/user", (req, res) -> new UserHandler(userService).register(req, res));
 
         Spark.awaitInitialization();
         return Spark.port();
