e1084bbc4b41b9db67133b4b27bdd20e6e707bbb
Jacob Hale <Jchale23@byui.edu>
2025-03-03 14:19:07 -0700

    data access

---
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -0,0 +1,27 @@
+package dataaccess;
+
+import model.UserData;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryUserDAO implements UserDAO {
+    private final Map<String, UserData> users = new HashMap<>();
+
+    @Override
+    public void insertUser(UserData user) throws DataAccessException{
+        if (users.containsKey(user.username())) {
+            throw new DataAccessException("Error: Username already taken");
+        }
+        users.put(user.username(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return users.get(username);
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        users.clear();
+    }
+}
