bf7ee82c9e0005888148d3e1dbbd3ec512218d2e
Jacob Hale <Jchale23@byui.edu>
2025-03-03 14:24:46 -0700

    data access

---
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -0,0 +1,29 @@
+package dataaccess;
+
+import model.AuthData;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryAuthDAO implements AuthDAO {
+    private final Map<String, AuthData> authTokens = new HashMap<>();
+
+    @Override
+    public void insertAuth(AuthData auth) throws DataAccessException {
+        authTokens.put(auth.authToken(), auth);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return authTokens.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        authTokens.remove(authToken);
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        authTokens.clear();
+    }
+}
