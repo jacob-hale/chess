a889c44346d7198c50a90762a4a302dc3571aaa3
Jacob Hale <Jchale23@byui.edu>
2025-03-03 20:34:09 -0700

    code quality fix

--- server/src/main/java/server/ErrorHandler.java
+++ server/src/main/java/server/ErrorHandler.java
@@ -6,13 +6,13 @@ import dataaccess.DataAccessException;
 
 
 public class ErrorHandler {
-    private static final Gson gson = new Gson();
+    private static final Gson GSON = new Gson();
     public static String handleDataAccessException(DataAccessException e, Response res) {
         if (e.getMessage().contains("Unauthorized")) {
             res.status(401);
         } else {
             res.status(500);
         }
-        return gson.toJson(new UserHandler.ErrorResponse(e.getMessage()));
+        return GSON.toJson(new UserHandler.ErrorResponse(e.getMessage()));
     }
 }
